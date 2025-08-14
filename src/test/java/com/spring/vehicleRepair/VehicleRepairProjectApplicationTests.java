d67c394cbebd70c56530673d8a0be42b5c006d90
Reyes Miguel Castro <miguel.castro.reyes@hotmail.com>
2025-08-13 21:14:11 -0600

    initial commit

---
+++ src/test/java/com/spring/vehicleRepair/VehicleRepairProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.vehicleRepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRepairProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
